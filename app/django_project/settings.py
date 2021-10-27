93f8947a140628a17920157daf2064711a01e577
SLCKDM <43304005+LevenusSupremus@users.noreply.github.com>
2021-10-28 00:34:07 +0300

    ya

--- app/django_project/settings.py
+++ app/django_project/settings.py
@@ -12,10 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from os import environ
+# from app.django_project.settings import DEBUG
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -28,16 +28,16 @@ DEBUG = int(environ.get('DEBUG', default=0))
 
 ALLOWED_HOSTS = environ.get('ALLOWED_HOSTS').split(' ')
 
-
 # Application definition
-
 INSTALLED_APPS = [
+    'psycopg2',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'recipes.apps.RecipesConfig',
 ]
 
 MIDDLEWARE = [
